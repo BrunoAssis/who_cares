f9068eeb8eab606573b82d2c249271e5d0b1ef3a
Bruno Assis <brunoassis@gmail.com>
2013-05-13 17:12:55 -0300

    Fully working (but ugly).

--- background.js
+++ background.js
@@ -1,12 +1,32 @@
-var get_hidden_pages = function() {
-  var hidden_pages_string = localStorage["wc_hidden_pages"];
-  var hidden_pages = [];
+var pages_to_hide = [];
+var pagenames = [];
 
-  if (hidden_pages_string && hidden_pages_string != undefined && hidden_pages_string != "undefined") {
-    hidden_pages = JSON.parse(hidden_pages_string);
-  }
+var hide_pages = function() {
+  chrome.storage.sync.get(['wc_hidden_pages_links', 'wc_hidden_pages_names'], function(items){
+    $('ul#home_stream').off('DOMSubtreeModified', hide_pages);
 
-  return hidden_pages;
+    if (items["wc_hidden_pages_links"]) {
+      pages_to_hide = items["wc_hidden_pages_links"];
+      pagenames = items["wc_hidden_pages_names"];
+    }
+
+    var selector = 'ul#home_stream li.uiStreamStory div.storyContent div.storyInnerContent div.mainWrapper div h5.uiStreamHeadline a[data-ft="{"tn":"P"}"]:not(.wc_processed)';
+    $(selector).each(function(){
+      var wc_link_el = $('<a href="#" class="wc_hide_page" data-page-name="' + $(this).html() + '" data-page-link="' + get_sanitized_link($(this).attr('href')) + '">WC?</a>');
+      $(this).after(wc_link_el);
+      $(this).addClass('wc_processed');
+
+      wc_link_el.click(add_hidden_page);
+
+      if (pages_to_hide) {
+        if ($.inArray(get_sanitized_link($(this).attr('href')), pages_to_hide) >= 0) {
+          hide_page($(this));
+        }
+      }
+    });
+
+    $('ul#home_stream').on('DOMSubtreeModified', hide_pages);
+  });
 }
 
 var get_sanitized_link = function(link) {
@@ -18,43 +38,27 @@ var get_sanitized_link = function(link) {
 }
 
 var add_hidden_page = function() {
-  var hidden_pages = get_hidden_pages();
-  var page_to_hide = $(this).attr('data-page');
+  var page_to_hide = $(this).attr('data-page-link');
+  var pagename     = $(this).attr('data-page-name');
 
-  if ($.inArray(page_to_hide, hidden_pages) == -1) {
-    hidden_pages.push(page_to_hide);
+  if ($.inArray(page_to_hide, pages_to_hide) == -1) {
+    pages_to_hide.push(page_to_hide);
+    pagenames.push(pagename);
   }
 
-  localStorage["wc_hidden_pages"] = JSON.stringify(hidden_pages);
+  chrome.storage.sync.set({
+    'wc_hidden_pages_links': pages_to_hide,
+    'wc_hidden_pages_names': pagenames
+  });
 
   hide_page($(this));
 }
 
 var hide_page = function(el) {
   hidable = el.parents('li.uiUnifiedStory');
-  hidable.css('background-color', '#ffa');
-  //hidable.hide();
-}
-
-var hide_all_pages = function() {
-  var hidden_pages = get_hidden_pages();
-
-  var selector = 'ul#home_stream li.uiStreamStory div.storyContent div.storyInnerContent div.mainWrapper div h5.uiStreamHeadline a[data-ft="{"tn":"P"}"]:not(.wc_processed)';
-  $(selector).each(function(){
-    var wc_link_el = $('<a href="#" class="wc_hide_page" data-page="' + get_sanitized_link($(this).attr('href')) + '">WC?</a>');
-    $(this).after(wc_link_el);
-    $(this).addClass('wc_processed');
-    $(wc_link_el).click(add_hidden_page);
-
-    if (hidden_pages) {
-      if ($.inArray(get_sanitized_link($(this).attr('href')), hidden_pages) >= 0) {
-        hide_page($(this));
-      }
-    }
-  });
+  hidable.hide();
 }
 
 $(document).ready(function() {
-  hide_all_pages();
-  //$('ul#home_stream').on('DOMSubtreeModified', hide_pages);
+  hide_pages();
 });
--- options.js
+++ options.js
@@ -1,3 +1,50 @@
+var pages_to_hide = [];
+var pagenames = [];
+
+var remove_hidden_page = function() {
+  var page_to_hide = $(this).attr('data-page-link');
+  var pagename     = $(this).attr('data-page-name');
+
+  if ($.inArray(page_to_hide, pages_to_hide) > -1) {
+    pages_to_hide = $.grep(pages_to_hide, function(value) {
+      return value != page_to_hide;
+    });
+
+    pagenames = $.grep(pagenames, function(value) {
+      return value != pagename;
+    });
+  }
+
+  chrome.storage.sync.set({
+    'wc_hidden_pages_links': pages_to_hide,
+    'wc_hidden_pages_names': pagenames
+  }, function() {
+    load_page_list();
+  });
+}
+
+var load_page_list = function() {
+  $('#pageList').empty();
+  chrome.storage.sync.get(['wc_hidden_pages_links', 'wc_hidden_pages_names'], function(items){
+    if (items["wc_hidden_pages_links"]) {
+      pages_to_hide = items["wc_hidden_pages_links"];
+      pagenames = items["wc_hidden_pages_names"];
+    }
+
+    for (i=0; i < pages_to_hide.length; i++) {
+      var list_item = $('<li></li>');
+      list_item.append('<span>' + pagenames[i] + ' | </span>');
+
+      var link_el = $('<a href="#" data-page-name="' + pagenames[i] + '" data-page-link="' + pages_to_hide[i] + '">Show again</a>');
+      link_el.on('click', remove_hidden_page);
+
+      list_item.append(link_el);
+
+      $('#pageList').append(list_item);
+    }
+  });
+}
+
 $(document).ready(function(){
-  var hiddenPages = get_hidden_pages();
+  load_page_list();
 });
